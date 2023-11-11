65a5405c7cd38a8a2c08e034ac289351160d4c58
Pur3-MAlice <aliceford98@live.co.uk>
2023-11-11 21:41:14 +0000

    state stuff

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreetingWithProps greeting="Nice to meet you" name="Bitch" age="25"/>
+      <FunctionalGreetingWithProps greeting="Nice to meet you" name="Bitch" age="25" />
+      <StatefulGreeting thingy="Crazy stateful timez" name="whore" />
     </div>
   );
 }
